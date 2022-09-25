d351dfb3a333501cb4a048f4a60608ab4059e41b
Dandy Naufaldi <dandy.naufaldi@gojek.com>
2022-09-25 15:40:20 +0700

    Add spec Grid Height when initialized with 3x3 returns 3

--- conway/model/grid.go
+++ conway/model/grid.go
@@ -1,13 +1,26 @@
 package model
 
 type Grid struct {
-
+	state  map[int]map[int]Void
+	height int
 }
 
-func NewGrid(initialState [][]bool) Grid {
-	return Grid{}
+type Void struct{}
+
+func NewGrid(initialState map[int]map[int]Void) Grid {
+	height := 0
+	for row := range initialState {
+		if row+1 > height {
+			height = row + 1
+		}
+	}
+
+	return Grid{
+		state:  initialState,
+		height: height,
+	}
 }
 
 func (g Grid) Height() int {
-	return 2
+	return g.height
 }
--- conway/model/grid_test.go
+++ conway/model/grid_test.go
@@ -8,12 +8,23 @@ import (
 
 func TestGrid_Height(t *testing.T) {
 	t.Run("when initialized with 2x3 returns 2", func(t *testing.T) {
-		twoByThree := [][]bool{
-			{true, true, true},
-			{true, true, true},
+		twoByThree := map[int]map[int]Void{
+			0: {0: Void{}, 1: Void{}, 2: Void{}},
+			1: {0: Void{}, 1: Void{}, 2: Void{}},
 		}
 		grid := NewGrid(twoByThree)
 
 		assert.Equal(t, 2, grid.Height())
+	})
+
+	t.Run("when initialized with 3x3 returns 3", func(t *testing.T) {
+		threeByThree := map[int]map[int]Void{
+			0: {0: Void{}, 1: Void{}, 2: Void{}},
+			1: {0: Void{}, 1: Void{}, 2: Void{}},
+			2: {0: Void{}, 1: Void{}, 2: Void{}},
+		}
+		grid := NewGrid(threeByThree)
+
+		assert.Equal(t, 3, grid.Height())
 	})
 }
