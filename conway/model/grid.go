fc3909332b152ad59d8d5222e2b8bac2e7fae881
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2022-10-01 20:59:07 +0700

    Add spec Grid NeighbourCount when bottom right cell is alive returns 1

--- conway/model/grid.go
+++ conway/model/grid.go
@@ -72,6 +72,7 @@ func (g Grid) NeighbourCount(row, column int) int {
 		{row: 0, column: +1},
 		{row: +1, column: -1},
 		{row: +1, column: 0},
+		{row: +1, column: +1},
 	}
 
 	for _, direction := range directions {
